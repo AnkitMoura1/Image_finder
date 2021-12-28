264a1dddfa61c734d323232c4ef8c1a6bc3ad481
Ankit <ankit2moura@gmail.com>
2021-12-28 10:50:58 +0530

    downloading and opening images from wikipedia using search button

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+import wikipedia
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -7,7 +9,16 @@ Builder.load_file("Frontend.kv")
 
 class FirstScreen(Screen):
     def search_image(self):
-        self.manager.current_screen.ids.img.source = "img.png"
+        image_name = self.manager.current_screen.ids.image_name.text
+        print(image_name)
+        page = wikipedia.page(image_name)
+        image_link = page.images[0]
+        print(image_link)
+        req = requests.get(image_link)
+        image_path = 'files/img2.jpg'
+        with open(image_path, 'wb') as f:
+            f.write(req.content)
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
